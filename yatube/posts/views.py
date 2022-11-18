126b43736c1fc7aefba04efef51d25255d6a24c1
Anastazise <anastazidevilfor@yandex.ru>
2022-11-18 12:00:59 +0300

    finish layout

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,22 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):    
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Главная страница',
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
     template = 'posts/group_list.html' 
-    return HttpResponse(f'Мороженое номер {slug}')
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Группаы проекта Yatube',
+    }
+    return render(request, template, context)
